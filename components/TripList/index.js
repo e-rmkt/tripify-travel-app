2d296ce91b8d48bf512c36f7f9076043e8579067
Tobias Steinhagen <to.steinhagen@gmail.com>
2023-09-18 17:05:25 +0200

    refactor: delete const {_id} and change value for the key in TripList/index.js

--- components/TripList/index.js
+++ components/TripList/index.js
@@ -5,17 +5,14 @@ const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function TripList() {
   const { data: trips, isLoading } = useSWR(`/api/trips`, fetcher);
-  console.log(trips);
   if (!trips || isLoading) {
     return <>is Loading...</>;
   }
 
-  const { _id } = trips;
-
   return (
     <ul>
       {trips.map((trip) => (
-        <Trip key={_id} {...trip} />
+        <Trip key={trip._id} {...trip} />
       ))}
     </ul>
   );
