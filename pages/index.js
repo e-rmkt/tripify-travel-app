a79a9da16fdef68b9f53c78579145d4eb44d3fe4
Tobias Steinhagen <to.steinhagen@gmail.com>
2023-09-18 12:22:37 +0200

    feat: add fetch for the trip data to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,16 @@
+import useSWR from "swr";
+
+const fetcher = (...arg) => fetch(...args).then((res) => res.json());
+
 export default function HomePage() {
+  const { data: trip, isLoading } = useSWR(`/api/trips`, fetcher);
+
+  if (!trip || isLoading) {
+    return <>is Loading...</>;
+  }
+
+const {title, location.country, location.city, timePeriod.startDate, timePeriod.endDate, img} = trip;
+
   return (
     <div>
       <h1>TravelApp</h1>
