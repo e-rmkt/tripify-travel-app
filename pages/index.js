b0493c8faa2ff17d38e651e7b78191fe7db59f32
Tobias Steinhagen <to.steinhagen@gmail.com>
2023-09-18 15:31:23 +0200

    feat: update pages/index.js to render triplist

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,10 @@
-import useSWR from "swr";
-
-const fetcher = (...arg) => fetch(...args).then((res) => res.json());
+import TripList from "@/components/TripList";
 
 export default function HomePage() {
-  const { data: trip, isLoading } = useSWR(`/api/trips`, fetcher);
-
-  if (!trip || isLoading) {
-    return <>is Loading...</>;
-  }
-
-const {title, location.country, location.city, timePeriod.startDate, timePeriod.endDate, img} = trip;
-
   return (
     <div>
       <h1>TravelApp</h1>
+      <TripList />
     </div>
   );
 }
