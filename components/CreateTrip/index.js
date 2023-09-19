c8e5d34fcb9c722a90685e8d04ec509ba53326f0
Pia Lüling <p.lueling@gmail.com>
2023-09-19 15:29:46 +0200

    feat: add correct page link

--- components/CreateTrip/index.js
+++ components/CreateTrip/index.js
@@ -1,5 +1,5 @@
 import { StyledLink } from "./CreateTrip.styled";
 
 export default function CreateTrip() {
-  return <StyledLink href="">+</StyledLink>;
+  return <StyledLink href="/forms/TripForm">+</StyledLink>;
 }
