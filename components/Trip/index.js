41abacd25eeb35dffbf37f7d281fa770b374c98d
Eliza Romeikat <eliza-romeikat@gmx.de>
2023-09-19 10:47:41 +0200

    feat: add Link component to Trip

--- components/Trip/index.js
+++ components/Trip/index.js
@@ -1,14 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Trip({ title, img, location, timePeriod }) {
+export default function Trip({ title, img, location, timePeriod, _id }) {
   return (
-    <li>
-      <Image src={img} width={150} height={100} alt="Dummy Pic" />
-      {title}
-      {location.map((location) => `${location.country} ${location.city}`)}
-      {timePeriod.map(
-        (timePeriod) => `${timePeriod.startDate} ${timePeriod.endDate}`
-      )}
-    </li>
+    <Link href={`/trip/${_id}`}>
+      <li>
+        <Image src={img} width={150} height={100} alt="Dummy Pic" />
+        {title}
+        {location.map((location) => `${location.country} ${location.city}`)}
+        {timePeriod.map(
+          (timePeriod) => `${timePeriod.startDate} ${timePeriod.endDate}`
+        )}
+      </li>
+    </Link>
   );
 }
