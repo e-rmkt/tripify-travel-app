5299afb34d112bb895de8a308e4c1871e7d19c14
Anna Dick <anna_dick@outlook.de>
2023-09-26 11:25:37 +0200

    style: add global styling

--- styles.js
+++ styles.js
@@ -9,6 +9,36 @@ export default createGlobalStyle`
 
   body {
     margin: 0;
+    padding: 0 20px;
     font-family: system-ui;
   }
+
+  a:link, 
+  a:visited {
+color: #252525;
+
+  }
+
+
+h2 {
+font-size: 16px;
+font-weight: 500;
+margin:0;
+}
+
+h3 {
+font-size: 14px;
+font-weight: 400;
+margin:0;
+}
+
+h4 {
+font-size: 12px;
+font-weight: 400;
+margin:0;
+}
+p {
+  
+}
+
 `;
