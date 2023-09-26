882a4c17e727d0386f3c4cd4597a202c6e7e6350
Anna Dick <anna_dick@outlook.de>
2023-09-26 12:03:30 +0200

    style: add styling to CreateTrip component

--- components/CreateTrip/CreateTrip.styled.js
+++ components/CreateTrip/CreateTrip.styled.js
@@ -2,12 +2,14 @@ import { styled } from "styled-components";
 import Link from "next/link";
 
 export const StyledLink = styled(Link)`
-  border: 0.125rem solid black;
+  position: absolute;
+  border: 0.125rem solid #ffa573;
+  background-color: #ffa573;
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 4.5rem;
-  height: 4.5rem;
+  width: 50px;
+  height: 50px;
   border-radius: 50%;
   font-size: 3.125rem;
   text-decoration: none;
