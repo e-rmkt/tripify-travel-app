16a8b623898bde907763f82ab981d3f55e4c2cfe
Anna Dick <anna_dick@outlook.de>
2023-09-27 10:16:02 +0200

    style: add styling for Text

--- components/Trip/Trip.styled.js
+++ components/Trip/Trip.styled.js
@@ -2,7 +2,7 @@ import { styled } from "styled-components";
 import Link from "next/link";
 import Image from "next/image";
 
-export const StyledList = styled.li`
+export const StyledListItem = styled.li`
   list-style-type: none;
   display: flex;
   gap: 10px;
@@ -24,4 +24,10 @@ export const StyledLink = styled(Link)`
 
 export const StyledImage = styled(Image)`
   border-radius: 10px;
+`;
+
+export const StyledText = styled.p`
+  font-size: 14px;
+  font-weight: 400;
+  margin: 0;
 `;
--- components/Trip/index.js
+++ components/Trip/index.js
@@ -1,18 +1,24 @@
 import Location from "../Location";
 import TimePeriod from "../TimePeriod";
-import { StyledList, Wrapper, StyledLink, StyledImage } from "./Trip.styled";
+import {
+  StyledListItem,
+  Wrapper,
+  StyledLink,
+  StyledImage,
+  StyledText,
+} from "./Trip.styled";
 
 export default function Trip({ title, img, location, timePeriod, _id }) {
   return (
     <StyledLink href={`/trip/${_id}`}>
-      <StyledList>
+      <StyledListItem>
         <StyledImage src={img} width={150} height={100} alt="Dummy Pic" />
         <Wrapper>
           <Location location={location} />
-          <h3>{title}</h3>
+          <StyledText>{title}</StyledText>
           <TimePeriod timePeriod={timePeriod} />
         </Wrapper>
-      </StyledList>
+      </StyledListItem>
     </StyledLink>
   );
 }
