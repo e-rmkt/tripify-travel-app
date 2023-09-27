e79714a5fcce3ec4b1caca9f8f6b61df85e597bf
Anna Dick <anna_dick@outlook.de>
2023-09-27 11:03:19 +0200

    refactor: make changes to Location and TimePeriod

--- components/Location/index.js
+++ components/Location/index.js
@@ -1,7 +1,14 @@
 export default function Location({ location }) {
   return (
     <h2>
-      {location.map((location) => `${location.city}, ${location.country}`)}
+      {location.map(
+        (location) =>
+          `${
+            !location.city
+              ? `${location.country}`
+              : `${location.city}, ${location.country}`
+          }`
+      )}
     </h2>
   );
 }
--- components/TimePeriod/index.js
+++ components/TimePeriod/index.js
@@ -2,7 +2,12 @@ export default function TimePeriod({ timePeriod }) {
   return (
     <time>
       {timePeriod.map(
-        (timePeriod) => `${timePeriod.startDate} - ${timePeriod.endDate}`
+        (timePeriod) =>
+          `${
+            !timePeriod.endDate
+              ? `${timePeriod.startDate}`
+              : `${timePeriod.endDate} - ${timePeriod.startDate}`
+          }`
       )}
     </time>
   );
