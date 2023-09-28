a4e06ae096f3fec8433dd7630c01fb2421e20b28
Anna Dick <anna_dick@outlook.de>
2023-09-28 11:24:06 +0200

    style: add LoadingSkeleton

--- components/TripList/TripList.styled.js
+++ components/TripList/TripList.styled.js
@@ -1,3 +1,4 @@
+import Skeleton from "react-loading-skeleton";
 import { styled } from "styled-components";
 
 export const StyledUnorderedList = styled.ul`
@@ -5,4 +6,17 @@ export const StyledUnorderedList = styled.ul`
   display: flex;
   flex-direction: column;
   padding: 0;
+`;
+
+export const LoadingSkeleton = styled(Skeleton)`
+  z-index: -1;
+  list-style-type: none;
+  display: flex;
+  gap: 10px;
+  width: 100%;
+  height: 113px;
+  background-color: #fff;
+  padding: 12px;
+  border-radius: 10px;
+  border: 1px solid rgba(68, 140, 255, 0.15);
 `;
--- components/TripList/index.js
+++ components/TripList/index.js
@@ -1,13 +1,15 @@
 import Trip from "../Trip";
 import useSWR from "swr";
-import { StyledUnorderedList } from "./TripList.styled";
+import { LoadingSkeleton, StyledUnorderedList } from "./TripList.styled";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function TripList() {
   const { data: trips, isLoading } = useSWR(`/api/trips`, fetcher);
+
   if (!trips || isLoading) {
-    return <h2>is Loading...</h2>;
+    return <LoadingSkeleton count={5} />;
   }
 
   return (
