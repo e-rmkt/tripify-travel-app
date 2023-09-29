a8562f8632ff318d33114119d2c07650b99473fd
Anna Dick <anna_dick@outlook.de>
2023-09-29 12:56:21 +0200

    style: add styling to Edit & Delete Button

--- components/DeleteButton/DeleteButton.styled.js
+++ components/DeleteButton/DeleteButton.styled.js
@@ -1,10 +1,14 @@
 import { styled } from "styled-components";
 
 export const StyledDeleteButton = styled.button`
+  display: flex;
+  justify-content: center;
+  gap: 8px;
+  padding: 6px 23px;
   border-radius: 13px;
   border: 1px solid #ff003d;
   background: #fff;
-  width: 127.208px;
+  width: 127px;
   height: 32px;
   color: #ff003d;
   font-size: 15px;
--- components/EditButton/EditButton.styled.js
+++ components/EditButton/EditButton.styled.js
@@ -2,9 +2,12 @@ import Link from "next/link";
 import styled from "styled-components";
 
 export const StyledEditButton = styled(Link)`
+  display: flex;
+  justify-content: center;
+  gap: 8px;
   border: 1px solid #448cff;
   background: #fff;
-  width: 117px;
+  width: 127px;
   height: 32px;
   padding: 6px 23px;
   border-radius: 13px;
