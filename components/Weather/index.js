28cab22e2e688443844e3fb6df8db11a73f9a5e6
Tobias Steinhagen <to.steinhagen@gmail.com>
2023-10-05 16:40:28 +0200

    feat: fetch and deconstruct the weather data

--- components/Weather/index.js
+++ components/Weather/index.js
@@ -4,7 +4,7 @@ const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function Weather() {
   const { data, isLoading } = useSWR(
-    `https://api.open-meteo.com/v1/forecast?latitude=53.5507&longitude=9.993&current_weather=true&timezone=GMT`,
+    `https://api.open-meteo.com/v1/forecast?latitude=53.5507&longitude=9.993&hourly=precipitation_probability,uv_index&daily=temperature_2m_max,sunrise,sunset,uv_index_max&timezone=GMT&forecast_days=3`,
     fetcher
   );
 
@@ -12,13 +12,27 @@ export default function Weather() {
     return <h2>Weather is not available</h2>;
   }
 
-  const { current_weather: weather, current_weather_units: unit } = data;
+  console.log(data);
+  console.log(data.daily.sunrise[0]);
 
-  return (
-    <>
-      Current weather
-      {weather.temperature}
-      {unit.temperature}
-    </>
-  );
+  const {
+    temperature_2m_max: temperatures,
+    sunrise: sunrises,
+    sunset: sunsets,
+    time: date,
+    uv_index_max: uv_index,
+  } = data.daily;
+
+  const {
+    sunrise: sunsrise_time,
+    sunset: sunset_time,
+    temperature_2m_max: temperature_unit,
+  } = data.daily_units;
+
+  const { precipitation_probability } = data.hourly;
+
+  const { precipitation_probability: precipitation_probability_unit } =
+    data.hourly_units;
+
+  return <></>;
 }
