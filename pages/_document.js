0d7d306473f376b60db479f249c58076afd6f2e2
Anna Dick <anna_dick@outlook.de>
2023-10-05 12:10:40 +0200

    feat: add modal root to document.js

--- pages/_document.js
+++ pages/_document.js
@@ -30,6 +30,7 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <div id="modal-root"></div>
         </body>
       </Html>
     );
