7cd83fde03e5ab06faf96279d48c5f33991d588a
Tobias Steinhagen <to.steinhagen@gmail.com>
2023-10-05 16:38:48 +0200

    refactor: clean and adjust handleAddTrip

--- pages/forms/TripForm/index.js
+++ pages/forms/TripForm/index.js
@@ -13,18 +13,13 @@ export default function TripFormPage() {
     setEndDateDisabled(!event.target.value);
   }
 
-  async function handleAddTrip(event) {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-    const tripData = Object.fromEntries(formData);
-
-    const repTripDataCountry = tripData.country.replace(" ", "");
+  async function handleAddTrip(tripData) {
+    const repTripDataCountry = tripData.country.name.replace(" ", "");
     const repTripDataCity = tripData.city.replace(" ", "-");
 
     const newTrip = {
       title: tripData.title,
-      location: [{ country: tripData.country, city: tripData.city }],
+      location: [{ country: tripData.country.name, city: tripData.city }],
       timePeriod: [
         { startDate: tripData.startDate, endDate: tripData.endDate },
       ],
@@ -35,23 +30,24 @@ export default function TripFormPage() {
 
     if (tripData.endDate < tripData.startDate) {
       alert("The end date needs to be bigger than the start date!");
-    } else {
-      const response = await fetch("/api/trips", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newTrip),
-      });
-
-      if (!response.ok) {
-        console.error(response.status);
-        return;
-      }
-
-      mutate();
-      router.push("/");
+      return;
     }
+
+    const response = await fetch("/api/trips", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTrip),
+    });
+
+    if (!response.ok) {
+      console.error(response.status);
+      return;
+    }
+
+    mutate();
+    router.push("/");
   }
 
   return (
