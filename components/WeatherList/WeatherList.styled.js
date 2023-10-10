7c99cd840aaef9b9d551e8d87fb8ca530f174869
Eliza Romeikat <eliza-romeikat@gmx.de>
2023-10-10 16:12:08 +0200

    style: add styling to weather button

---
+++ components/WeatherList/WeatherList.styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+  background-color: #448cff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-self: center;
+  gap: 8px;
+  border-style: none;
+  height: 32px;
+  width: fit-content;
+  padding: 6px 23px;
+  border-radius: 13px;
+  color: #fff;
+  font-size: 15px;
+  font-weight: 500;
+  text-wrap: nowrap;
+`;
