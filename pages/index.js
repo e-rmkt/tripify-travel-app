c0c2b571c2ed4e3ad05e11907c07a27d840d7ebd
Tobias Steinhagen <to.steinhagen@gmail.com>
2023-10-13 11:53:30 +0200

    refacotr: clean the code

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 import CreateTrip from "@/components/CreateTrip";
 import TripList from "@/components/TripList";
-import { Wrapper, StyledLink } from "@/components/Layout/Layout.styled";
+import { Wrapper } from "@/components/Layout/Layout.styled";
 import MapIcon from "@/components/Layout/MapIcon.svg";
 import Link from "next/link";
 
 export default function HomePage() {
   return (
     <main>
-      {/* <h1>My Trips</h1> */}
       <Wrapper>
         <h1>My Trips</h1>
         <Link href="/worldMap">
